fcc37fe7ec35babe2c6ec05a5837680f4fd34746
Kervin Vasquez <kervinlvh@gmail.com>
2019-05-24 14:22:22 -0700

    Install JEST and ENZYME

---
+++ __tests__/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {mount} from 'enzyme';
+// import IndexPage from '../pages/index'
+
+
+    it('should render without throwing an error',  () => {
+   
+        expect(1).toBe(1);
+    })
+ 
