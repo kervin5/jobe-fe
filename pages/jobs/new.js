1c59679b2cb27574bf0b1efb69abb02297bbc78b
Kervin Vasquez <kervinlvh@gmail.com>
2019-05-30 10:45:40 -0700

    Fix styling

--- pages/jobs/new.js
+++ pages/jobs/new.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import classes from './new.module.scss';
-import PageSection from '../../components/common/Layout/PageSection/PageSection';
+
 import Layout from '../../components/common/Layout/Layout';
+import PageSection from '../../components/common/Layout/PageSection/PageSection';
+import classes from './new.module.scss';
 import JobCreator from '../../components/jobs/JobCreator/JobCreator.js';
+
 
 const homePage = (props) => {
     return (
