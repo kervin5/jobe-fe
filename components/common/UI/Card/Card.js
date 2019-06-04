a44907d7bf0cbad4032a86bd46e1b6d50c653d4c
Kervin Vasquez <kervinlvh@gmail.com>
2019-06-04 14:33:27 -0700

    Refactor: Refactor card and Bubble styled components

--- components/common/UI/Card/Card.js
+++ components/common/UI/Card/Card.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import classes from './Card.module.scss';
+// import classes from './Card.module.scss';
 
 const card = props => {
+    const animateHover = props.animate ? "1.05" : "1";
 
     return(
-        <div className={classes.Card + " " +  (props.className ? props.className : "")}>
+        <div>
             {props.children}
+            <style jsx>{`
+                div {
+                    box-shadow: 0px 0px 6px 1px rgba(0, 0, 0, 0.15);
+                    padding: 10px;
+                    border-radius: 5px;
+                    ${props.styles || ""}
+                }
+
+                div:hover {
+                    transform: scale(${animateHover});
+                }
+            `}</style>
         </div>
     );
 };
--- components/common/UI/NavigationItems/NavigationItem/NavigationItem.js
+++ components/common/UI/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,8 +3,26 @@ import React from 'react';
 
 const navigationItem = () => {
     return (
-        <div className={classes.NavigationItem}>
+        <div >
             Test
+            <style jsx>{`
+                div {
+                    background-color: transparent;
+                    height: 100%;
+                    transition: 250ms;
+                  
+                    padding-left: 20px;
+                    padding-right: 20px;
+                    padding-top: 15px;
+                    transition-timing-function: ease-in-out;
+                }
+
+                div:hover {
+                    background-color: white;
+                    border-bottom: 5px solid orangered;
+                }
+                
+            `}</style>
         </div>
     );
 };
