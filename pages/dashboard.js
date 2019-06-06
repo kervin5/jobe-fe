fd5ba14632078d350337c64522fa81f2941b725a
Kervin Vasquez <kervinlvh@gmail.com>
2019-06-06 08:24:26 -0700

    Handle submit event of login form

---
+++ pages/dashboard.js
@@ -0,0 +1,18 @@
+import Layout from '../components/common/Layout/Layout';
+import PageSection from '../components/common/Layout/PageSection';
+
+
+const homePage = (props) => {
+    return (
+        <Layout title={"Home Page"}  hideNav>
+            <PageSection className="HomePage" column fullHeight>
+              
+            </PageSection>
+            <style jsx>{`
+
+            `}</style>
+        </Layout>
+    );
+};
+
+export default homePage;
