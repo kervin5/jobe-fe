3fb653c50d2710ee7c3226d62938d45b2764419d
Kervin Vasquez <kervinlvh@gmail.com>
2019-06-11 08:39:35 -0700

    TODO: debug login

--- components/users/LoginForm/LoginForm.js
+++ components/users/LoginForm/LoginForm.js
@@ -45,13 +45,28 @@ const loginForm = () => {
         if(email.valid && password.valid) {
             try {
                 // const result = await axios.post('https://myexactjobsapi.herokuapp.com/api/auth',{email: email.value, password: password.value, withCredentials: true});
-                const result = await axios.post('/auth',{email: email.value, password: password.value},{withCredentials: true});
-                console.log(result); 
+                const result = await axios({ 
+                    method: 'post',
+                    url: '/auth',
+                    data: {
+                        email: email.value, password: password.value
+                    },
+                    withCredentials: true
+                });
+
+                // postData('https://myexactjobsapi.herokuapp.com/api/auth',{
+                //     email: email.value, password: password.value
+                // });
+                console.log(result);
             }
             catch(ex) {
                 console.log("Error",ex.response);
             }
         }
+
+        
+
+        
     }  
 
     const fieldsToRender = Object.keys(formData).map(key=>{
@@ -93,5 +108,24 @@ const loginForm = () => {
     </React.Fragment>
     );
 }
+
+function postData(url = '', data = {}) {
+    // Default options are marked with *
+      return fetch(url, {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors', // no-cors, cors, *same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'include', // include, *same-origin, omit
+          headers: {
+              'Content-Type': 'application/json',
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        //  redirect: 'follow', // manual, *follow, error
+          //referrer: 'no-referrer', // no-referrer, *client
+          body: JSON.stringify(data), // body data type must match "Content-Type" header
+      })
+      .then(response => response.json()); // parses JSON response into native Javascript objects 
+  }
+
 
 export default loginForm;
