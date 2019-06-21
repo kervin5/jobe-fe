039956fa17948d67ee33c85a1f1b9ac611bf8263
Kervin Vasquez <kervinlvh@gmail.com>
2019-06-21 09:21:04 -0700

    Install prettier

--- components/jobs/Search/SearchForm.js
+++ components/jobs/Search/SearchForm.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
-import variables from '../../common/globalVariables';
-import axios from 'axios';
+import React, { useState } from "react";
+import variables from "../../common/globalVariables";
+import axios from "axios";
 // import classes from './SearchForm.module.scss';
-import InputField from '../../common/UI/Input/InputField';
-import Button from '../../common/UI/Button';
+import InputField from "../../common/UI/Input/InputField";
+import Button from "../../common/UI/Button";
 
 const buttonStyles = `margin-top:10px;`;
 const inputStyles = `min-width: 300px`;
@@ -12,51 +12,66 @@ const searchForm = props => {
   const [searchTerms, setSearchTerms] = useState("");
   const [searchLocation, setSearchLocation] = useState("");
 
-  const submitFormHandler = (e) => {
+  const submitFormHandler = e => {
     e.preventDefault();
-    const formData = {searchTerms, searchLocation};
-
-    axios.post('http://google.com',formData);
+    const formData = { searchTerms, searchLocation };
+    axios.post("http://google.com", formData);
   };
 
-  return(
-      <form >
-          <InputField type="text" placeholder="Job Title, Keywords, or Company Name" rounded centerPlaceholder icon="search" value={searchTerms} change={setSearchTerms}/>
-          <InputField type="text" placeholder="Location" rounded icon="map-marker-alt"value={searchLocation} change={setSearchLocation} />
-          <Button styles={buttonStyles} click={submitFormHandler}>Search</Button>
-          <style jsx >{`
-            form {
-              width: 100%;
-              max-width: 400px;
-            }
+  return (
+    <form>
+      <InputField
+        type="text"
+        placeholder="Job Title, Keywords, or Company Name"
+        rounded
+        centerPlaceholder
+        icon="search"
+        value={searchTerms}
+        change={setSearchTerms}
+      />
+      <InputField
+        type="text"
+        placeholder="Location"
+        rounded
+        icon="map-marker-alt"
+        value={searchLocation}
+        change={setSearchLocation}
+      />
+      <Button styles={buttonStyles} click={submitFormHandler}>
+        Search
+      </Button>
+      <style jsx>{`
+        form {
+          width: 100%;
+          max-width: 400px;
+        }
 
-            form * {
-              width: 100%;
-            }
-            
-            form :global(.InputContainer) {
-                margin-left: 5px;
-                margin-right: 5px;
-            }
-            
-            form :global(button) {
-                margin-left: 5px;
-                margin-right: 5px;
-            }
+        form * {
+          width: 100%;
+        }
 
-            @media (min-width: ${variables.mediumScreen}) {
+        form :global(.InputContainer) {
+          margin-left: 5px;
+          margin-right: 5px;
+        }
 
-              form {
-                display: flex;
-                max-width: 920px;
-              }
-          
-              form > * {
-                margin: 4px;
-              }
-            }
-          `}</style>
-      </form>
+        form :global(button) {
+          margin-left: 5px;
+          margin-right: 5px;
+        }
+
+        @media (min-width: ${variables.mediumScreen}) {
+          form {
+            display: flex;
+            max-width: 920px;
+          }
+
+          form > * {
+            margin: 4px;
+          }
+        }
+      `}</style>
+    </form>
   );
 };
 
