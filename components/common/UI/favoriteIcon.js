b59ff63c7e6e322d2775b81e042b88780987400d
Daniel <daniel.jimenez.619>
2019-07-01 14:36:01 -0700

    fixing not staged changes

--- components/common/UI/favoriteIcon.js
+++ components/common/UI/favoriteIcon.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 const favoriteIcon = props => {
   const [favoriteSelected, setFavoriteSelected] = useState(false);
   let iconColor = { color: "#ffffff" };
