e695351cc98d2edf3d652394be9bfc8dcf0c9e56
Kervin <kervinlvh@gmail.com>
2019-07-03 10:18:54 -0700

    Modify server

--- server.js
+++ server.js
@@ -4,16 +4,34 @@ const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
 const express = require("express");
 const compression = require("compression");
-
+const fs = require("fs");
 // const {createServer} = require('http');
 // app.prepare().then(() => {
 //   createServer(handler).listen( process.env.PORT || 3000);
 // });
 
+const isDev = process.env.NODE_ENV !== "production";
+const isProd = !isDev;
+const buildId = isProd
+  ? fs.readFileSync("./.next/BUILD_ID", "utf8").toString()
+  : null;
+
 app.prepare().then(() => {
   const server = express();
-
   server.use(handler);
+
+  if (isProd) {
+    server.get("/_next/-/app.js", (req, res) =>
+      app.serveStatic(req, res, path.resolve("./.next/app.js"))
+    );
+
+    const hash = buildId;
+
+    server.get(`/_next/${hash}/app.js`, (req, res) =>
+      app.serveStatic(req, res, path.resolve("./.next/app.js"))
+    );
+  }
+
   server.use(
     compression({
       filter: function() {
