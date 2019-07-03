50b49f047084ad7c9082c810bdc37838b85d780c
Kervin <kervinlvh@gmail.com>
2019-07-03 09:40:29 -0700

    Modify staging

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
 const express = require("express");
 const compression = require("compression");
-const expressApp = express();
+const server = express();
 
 // const {createServer} = require('http');
 // app.prepare().then(() => {
 //   createServer(handler).listen( process.env.PORT || 3000);
 // });
-if (process.env.NODE_ENV === "production") {
-  expressApp.use(compression);
-}
 
 app.prepare().then(() => {
-  expressApp.use(handler).listen(process.env.PORT || 3000);
+  server.use(handler);
+
+  if (process.env.NODE_ENV === "production") {
+    server.use(compression);
+  }
+
+  server.listen(process.env.PORT || 3000);
 });
