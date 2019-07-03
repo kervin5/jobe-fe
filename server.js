af94120c79fb6ce59b22204c6970b87103173759
Kervin <kervinlvh@gmail.com>
2019-07-03 09:54:44 -0700

    Refactor compression order

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
 const express = require("express");
 const compression = require("compression");
-const server = express();
 
 // const {createServer} = require('http');
 // app.prepare().then(() => {
@@ -12,6 +11,8 @@ const server = express();
 // });
 
 app.prepare().then(() => {
+  const server = express();
+
   server.use(handler);
   server.use(compression());
   server.listen(process.env.PORT || 3000);
