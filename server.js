be367162fecfe36326b089bac070da0381d8258d
Kervin <kervinlvh@gmail.com>
2019-07-03 10:31:27 -0700

    Modify server

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const next = require("next");
 const routes = require("./routes");
 const app = next({ dev: process.env.NODE_ENV !== "production" });
-const handler = routes.getRequestHandler(app);
+// const handler = routes.getRequestHandler(app);
 const express = require("express");
 const compression = require("compression");
+const handle = app.getRequestHandler();
 
 // const {createServer} = require('http');
 // app.prepare().then(() => {
@@ -13,6 +14,9 @@ const compression = require("compression");
 app.prepare().then(() => {
   const server = express();
 
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
   // server.use(handler);
   server.use(compression());
   server.listen(process.env.PORT || 3000);
