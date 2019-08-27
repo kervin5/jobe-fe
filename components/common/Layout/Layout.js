0e6ab02da61fb31df10c47e587f35114eaf8cfa7
Kervin Vasquez <kervinlvh@gmail.com>
2019-08-27 16:51:06 -0700

    Install nprogress

--- components/common/Layout/Layout.js
+++ components/common/Layout/Layout.js
@@ -1,8 +1,23 @@
 import React, { useState } from "react";
-//import Head from "../../head.js";
+import Router from "next/router";
+import NProgress from "nprogress";
 import NavigationBar from "../UI/Navigation/NavigationBar";
 // import NavigationDrawer from "../UI/Navigation/NavigationDrawer";
 import variables from "../globalVariables";
+
+Router.onRouteChangeStart = () => {
+  console.log("Started");
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+  console.log("Completed");
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  console.log("Error");
+  NProgress.done();
+};
 
 const layout = props => {
   const [showDrawer, setShowDrawer] = useState(false);
@@ -28,10 +43,6 @@ const layout = props => {
           overflow-x: hidden;
         }
 
-        .MuiSvgIcon-root {
-          color: ${variables.accentColor1};
-        }
-
         p {
           line-height: 1.6em;
         }
@@ -46,6 +57,88 @@ const layout = props => {
 
         fieldset {
           border: none;
+        }
+
+        /* Make clicks pass-through */
+        #nprogress {
+          pointer-events: none;
+        }
+
+        #nprogress .bar {
+          background: ${variables.accentColor1};
+          position: fixed;
+          z-index: 1031;
+          top: 0;
+          left: 0;
+
+          width: 100%;
+          height: 5px;
+        }
+
+        /* Fancy blur effect */
+        #nprogress .peg {
+          display: block;
+          position: absolute;
+          right: 0px;
+          width: 100px;
+          height: 100%;
+          box-shadow: 0 0 10px ${variables.accentColor1},
+            0 0 5px ${variables.accentColor1};
+          opacity: 1;
+
+          -webkit-transform: rotate(3deg) translate(0px, -4px);
+          -ms-transform: rotate(3deg) translate(0px, -4px);
+          transform: rotate(3deg) translate(0px, -4px);
+        }
+
+        /* Remove these to get rid of the spinner */
+        #nprogress .spinner {
+          display: block;
+          position: fixed;
+          z-index: 1031;
+          bottom: 15px;
+          left: 15px;
+        }
+
+        #nprogress .spinner-icon {
+          width: 18px;
+          height: 18px;
+          box-sizing: border-box;
+
+          border: solid 2px transparent;
+          border-top-color: ${variables.accentColor1};
+          border-left-color: ${variables.accentColor1};
+          border-radius: 50%;
+
+          -webkit-animation: nprogress-spinner 400ms linear infinite;
+          animation: nprogress-spinner 400ms linear infinite;
+        }
+
+        .nprogress-custom-parent {
+          overflow: hidden;
+          position: relative;
+        }
+
+        .nprogress-custom-parent #nprogress .spinner,
+        .nprogress-custom-parent #nprogress .bar {
+          position: absolute;
+        }
+
+        @-webkit-keyframes nprogress-spinner {
+          0% {
+            -webkit-transform: rotate(0deg);
+          }
+          100% {
+            -webkit-transform: rotate(360deg);
+          }
+        }
+        @keyframes nprogress-spinner {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
         }
       `}</style>
     </div>
