aad3774f96aa4a880b10efb4d9bb2076f6b97686
Kervin <kervinlvh@gmail.com>
2019-08-28 02:56:31 -0700

    Debug ssr

--- components/hoc/WithApollo.js
+++ components/hoc/WithApollo.js
@@ -1,3 +1,4 @@
+import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import fetch from "isomorphic-unfetch";
