22f172fc57a2bfe9bee7fa3f0e87e3682b548d44
Daniel <daniel.jimenez.619>
2019-09-04 15:28:43 -0700

    Resume viewer on me page

---
+++ components/common/UI/ResumeList.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { List, Button, Icon } from "semantic-ui-react";
+import moment from "moment";
+
+const resumeList = props => {
+  console.log(props.list);
+  return (
+    <List divided relaxed>
+      <List.Icon name="" size="large" verticalAlign="middle" />
+      {props.list.map((list, index) => {
+        return (
+          <List.Item key={list.id}>
+            <List.Content>
+              <List.Header as="a">
+                <p>{list.file.id}</p>
+              </List.Header>
+              <List.Description as="a">
+                <p>
+                  Created: {moment(list.file.createdAt).format("MMM Do YYYY")}
+                </p>
+              </List.Description>
+
+              <Button
+                icon
+                labelPosition="left"
+                href={list.file.path}
+                target="_blank"
+                floated="right"
+                color="green"
+              >
+                <Icon name="download" />
+                Download
+              </Button>
+            </List.Content>
+          </List.Item>
+        );
+      })}
+    </List>
+  );
+};
+
+export default resumeList;
