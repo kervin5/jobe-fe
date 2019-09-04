b22d2608e881a7e633fae18008d88900a9a8468f
Kervin Vasquez <kervinlvh@gmail.com>
2019-09-03 17:40:46 -0700

    Implement Trigger for register pop up

--- components/common/UI/ApplyToJobButton.js
+++ components/common/UI/ApplyToJobButton.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import { Mutation, Query } from "react-apollo";
 import gql from "graphql-tag";
 import { Loader } from "semantic-ui-react";
-import PopUp from "./PopUp";
-import RegisterForm from "../../users/RegisterForm";
 import RegisterToApplyButton from "./RegisterToApplyButton";
 import Button from "./Button";
 
@@ -30,8 +28,6 @@ const CHECK_USER_APPLICATION_STATUS_QUERY = gql`
 `;
 
 const ApplyToJobButton = props => {
-  const [showPopUp, setShowPopUp] = useState(false);
-
   return (
     <Query
       query={CHECK_USER_APPLICATION_STATUS_QUERY}
@@ -83,10 +79,6 @@ const ApplyToJobButton = props => {
                 );
               }}
             </Mutation>
-
-            <PopUp show={showPopUp} changeHandler={setShowPopUp}>
-              <RegisterForm onSubmit={() => console.log("Applied")} />
-            </PopUp>
           </React.Fragment>
         );
       }}
--- components/common/UI/RegisterToApplyButton.js
+++ components/common/UI/RegisterToApplyButton.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
+import PopUp from "./PopUp";
+import RegisterForm from "../../users/RegisterForm";
 
 const RegisterToApplyButton = () => {
+  const [showPopUp, setShowPopUp] = useState(false);
+
   return (
-    <div>
-      <Button fullWidth> Apply Now 😀</Button>
-    </div>
+    <>
+      <div>
+        <Button fullWidth click={() => setShowPopUp(true)}>
+          {" "}
+          Apply Now 😀
+        </Button>
+      </div>
+      <PopUp show={showPopUp} changeHandler={setShowPopUp}>
+        <p>
+          Before applying for this position you need to register with us. It
+          will only take a few minutes 😊
+        </p>
+        <RegisterForm onSubmit={() => console.log("Applied")} />
+      </PopUp>
+    </>
   );
 };
 
