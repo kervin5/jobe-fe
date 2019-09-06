2fca094f240d4409728b1b386d7e7a3c5bc66ffd
Daniel <daniel.jimenez.619>
2019-09-06 13:16:19 -0700

    work on ApplicantPage

--- components/dashboard/DashboardHome/ApplicantPage.js
+++ components/dashboard/DashboardHome/ApplicantPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
@@ -6,13 +6,13 @@ import Table from "../../common/UI/Table";
 import Loader from "../../common/UI/Animated/Loader";
 
 const APPLICATION_COUNT_QUERY = gql`
-  query APPLICATION_COUNT_QUERY {
+  query APPLICATION_COUNT_QUERY($perPage: Int!, $skip: Int!) {
     me {
       id
       jobs {
         title
         id
-        applications {
+        applications(first: $perPage, skip: $skip) {
           id
           user {
             name
@@ -40,15 +40,45 @@ const APPLICATION_COUNT_QUERY = gql`
   }
 `;
 
+const USER_JOBS_CONNECTION_QUERY = gql`
+query USER_JOBS_CONNECTION_QUERY
+  jobsConnectionPerUser {
+    aggregate {
+      count
+    }
+  }
+`;
+
 const ApplicantPage = () => {
-  <Query query={APPLICATION_COUNT_QUERY}>
-    {({ error, loading, data }) => {
-      console.log(error, loading, data);
-      if (error) return <p>Something went wrong...</p>;
-      if (loading) return <Loader />;
-      return <p>Whats up!</p>;
-    }}
-  </Query>;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const turnPageHandler = pageNumber => {
+    setCurrentPage(parseInt(pageNumber));
+  };
+
+  return (
+    <Query query={USER_JOBS_CONNECTION_QUERY} ssr={false}>
+      {userApplicationData => {
+        if (userApplicationData.error) return <p>Something went wrong ...</p>;
+        if (userApplicationData.loading) return <Loader />;
+        return (
+          <Query
+            query={APPLICATION_COUNT_QUERY}
+            variables={{ perPage, skip: (currentPage - 1) * perPage }}
+          >
+            {({ error, loading, data }) => {
+              console.log(error);
+              if (error) return <p>Something went wrong...</p>;
+              if (loading) return <Loader />;
+              let applicantData = data.me.jobs[0];
+              console.log(applicantData);
+              return <Table data={applicantData} page />;
+            }}
+          </Query>
+        );
+      }}
+    </Query>
+  );
 };
 
 export default ApplicantPage;
