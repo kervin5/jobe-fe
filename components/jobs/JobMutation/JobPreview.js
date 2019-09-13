1b8f1d99e10ed4c320466b729e6f51f0a10e093d
Kervin Vasquez <kervinlvh@gmail.com>
2019-09-13 13:10:33 -0700

    Add edit or remove button

--- components/jobs/JobMutation/JobPreview.js
+++ components/jobs/JobMutation/JobPreview.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import SingleJobListing from "../JobListing/SingleJobListing";
+import EditOrContinuerButton from "./EditOrContinueButtons";
 
 export class JobPreview extends Component {
   render() {
-    return <SingleJobListing jobId={this.props.jobId} preview={true} />;
+    return (
+      <>
+        <EditOrContinuerButton jobId={this.props.jobId} />
+        <SingleJobListing jobId={this.props.jobId} preview={true} />
+      </>
+    );
   }
 }
 
