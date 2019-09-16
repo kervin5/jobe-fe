b400ffddeb36427a7c7c7cf721f9c2388fcc1286
Kervin Vasquez <kervinlvh@gmail.com>
2019-09-16 13:29:13 -0700

    Implement popular terms

---
+++ components/jobs/PopularTerms.js
@@ -0,0 +1,80 @@
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import DynamicImageBg from "../common/UI/DynamicImageBg";
+
+const POPULAR_TERMS_QUERY = gql`
+  query POPULAR_TERMS_QUERY {
+    popularTerms {
+      id
+      label
+      type
+    }
+  }
+`;
+
+const PopularTerms = () => {
+  return (
+    <div className="PopularTerms">
+      <Query query={POPULAR_TERMS_QUERY}>
+        {({ error, loading, data }) => {
+          if (error) return <p>Something went wrong</p>;
+          if (loading) return <p>Loading</p>;
+          if (data)
+            return data.popularTerms.map(term => (
+              <div key={term.id} className="Term">
+                <DynamicImageBg query={term.label}>
+                  <p className="TermContent">{term.label}</p>
+                </DynamicImageBg>
+              </div>
+            ));
+        }}
+      </Query>
+      <style jsx>
+        {`
+          .PopularTerms {
+            width: 100%;
+            display: grid;
+            grid-template-columns: auto auto auto;
+            grid-gap: 1em;
+          }
+
+          .Term {
+            border-radius: 12px;
+            overflow: hidden;
+            position: relative;
+            transition: 100ms;
+          }
+
+          .Term:hover {
+            transform: scale(1.05);
+          }
+
+          .Term:before {
+            content: "";
+            background: rgba(0, 0, 0, 0.2);
+            position: absolute;
+            display: block;
+            z-index: 5;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            top: 0;
+          }
+
+          .Term:nth-child(2n) {
+            flex-basis: 100%;
+          }
+
+          .Term .TermContent {
+            padding: 30px;
+            font-weight: bold;
+            color: white;
+            text-align: center;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default PopularTerms;
--- pages/jobs/[jid].js
+++ pages/jobs/[jid].js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-
+import gql from "graphql-tag";
+import redirect from "../../lib/redirect";
 import variables from "../../components/common/globalVariables";
 import Container from "../../components/common/Layout/Container";
 import SingleJobListing from "../../components/jobs/JobListing/SingleJobListing";
@@ -7,6 +8,15 @@ import SingleJobListing from "../../components/jobs/JobListing/SingleJobListing"
 import PageSection from "../../components/common/Layout/PageSection";
 
 const pageStyles = `background-color:${variables.mutedColor1};justify-content: center;`;
+
+const SINGLE_JOB_QUERY = gql`
+  query SINGLE_JOB_QUERY($id: ID!) {
+    job(where: { id: $id }) {
+      id
+      status
+    }
+  }
+`;
 
 const SingleJobView = props => {
   return (
@@ -18,10 +28,19 @@ const SingleJobView = props => {
   );
 }; //eof
 
-SingleJobView.getInitialProps = async function({ query }) {
+SingleJobView.getInitialProps = async function({ query, apolloClient, res }) {
   const { jid } = query;
   const slugParts = jid.split("-");
   const jobId = slugParts[slugParts.length - 1];
+
+  const jobData = await apolloClient.query({
+    query: SINGLE_JOB_QUERY,
+    variables: { id: jobId }
+  });
+
+  if (jobData.data.job.status !== "POSTED") {
+    redirect({ res }, "/");
+  }
 
   return { jobId };
 };
