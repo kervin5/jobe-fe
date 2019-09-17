9ac7d1a7f7a516d7d70c4c83095e31947b59848d
Daniel <daniel.jimenez.619>
2019-09-17 12:20:54 -0700

    removed imports/export from Container.js

--- components/common/Layout/Container.js
+++ components/common/Layout/Container.js
@@ -1,14 +1,13 @@
 import React from "react";
-import bottomNav from "../UI/Navigation/BottomNav";
+
 // import classes from './Container.module.scss';
-import BottomNav from "../UI/Navigation/BottomNav";
+
 const container = props => {
   // const customWidth = props.maxWidth || "900px";
 
   return (
     <div>
       {props.children}
-      <BottomNav />
       <style jsx>{`
         div {
           margin: 0 auto;
