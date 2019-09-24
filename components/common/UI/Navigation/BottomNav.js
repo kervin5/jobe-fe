a5128da170389ba0b7209556c7c0aff6ba33ff3c
Daniel <daniel.jimenez.619>
2019-09-24 10:05:22 -0700

    switched up icons

--- components/common/UI/Navigation/BottomNav.js
+++ components/common/UI/Navigation/BottomNav.js
@@ -14,15 +14,10 @@ const bottomNav = () => (
     </NavigationItem>
     <RenderIfLoggedOut>
       <NavigationItem href={"/user/login"}>
-        <Icon name="key" inverted color="grey" size="large" />
+        <Icon name="sign in" inverted color="grey" size="large" />
       </NavigationItem>
       <NavigationItem href={"/user/register"}>
-        <Icon
-          name="file alternate outline"
-          inverted
-          color="grey"
-          size="large"
-        />
+        <Icon name="signup" inverted color="grey" size="large" />
       </NavigationItem>
     </RenderIfLoggedOut>
     <RenderIfLoggedIn permissions={[{ object: "JOB", action: "CREATE" }]}>
