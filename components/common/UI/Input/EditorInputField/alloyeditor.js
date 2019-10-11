bf8f0d53de123fa046a165f597f8e5b5865d2b9f
Kervin Vasquez <kervinlvh@gmail.com>
2019-10-11 12:57:59 -0700

    Install alloyeditor

---
+++ components/common/UI/Input/EditorInputField/alloyeditor.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+import loaderAlloyEditor from "./loader-alloyeditor";
+
+class AlloyEditorComponent extends Component {
+  componentDidMount() {
+    const { container, alloyEditorConfig } = this.props;
+
+    loaderAlloyEditor()
+      .then(alloyeditor => {
+        this._editor = alloyeditor.editable(container, alloyEditorConfig);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  componentWillUnmount() {
+    this._editor.destroy();
+  }
+
+  render() {
+    const { children, container } = this.props;
+
+    return <div id={container}>{children}</div>;
+  }
+}
+
+export default AlloyEditorComponent;
