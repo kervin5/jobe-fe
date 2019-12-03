a12d783f3cf9e2365373ab01741186adda54988b
Exact Staff Git <admin@exactstaff.com>
2019-12-03 13:20:04 -0800

    Implement layout of user info

--- components/common/UI/Avatar.js
+++ components/common/UI/Avatar.js
@@ -1,17 +1,26 @@
 import React from "react";
-import variables from "../common/globalVariables";
+import variables from "../globalVariables";
 
 const Avatar = ({ name = "Kervin Vasquez" }) => {
   const initials = name.split(" ");
   const firstInitial = initials[0][0];
   const lastInitial = initials.pop()[0];
+
   return (
     <span className="Avatar">
       {`${firstInitial} ${lastInitial}`}
       <style jsx>{`
         .Avatar {
-          background: ${true ? variables.clearColor : "transparent"};
+          background: ${variables.accentColor3};
+          color: ${variables.clearColor};
           font-weight: bold;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 100px;
+          height: 100px;
+          border-radius: 50px;
+          font-size: 2em;
         }
       `}</style>
     </span>
--- components/me/UserInfo.js
+++ components/me/UserInfo.js
@@ -1,21 +1,44 @@
-import React from "react";
-import Title from "../common/UI/Title";
+import React, { useState, useEffect } from "react";
+import UserLocator from "../../data/UserLocator";
 import { Query } from "react-apollo";
+import { Button, Icon, Image, Item, Label } from "semantic-ui-react";
 import Card from "../common/UI/Card";
-import Avatar from "../users/Avatar";
+import Avatar from "../common/UI/Avatar";
 import { ME_USER_QUERY } from "../../lib/auth";
 
 const UserInfo = () => {
+  const [userLocation, setUserLocation] = useState("Loading");
+
+  useEffect(() => {
+    new UserLocator().getLocation().then(res => {
+      setUserLocation(res.name);
+    });
+  }, []);
+
   return (
     <div className="UserInfo">
       <Card withBackground>
         <Query query={ME_USER_QUERY}>
           {({ error, loading, data }) => {
-            console.log(data);
             return (
               <>
-                <Avatar />
-                <Title>{data ? data.me.name : ""}</Title>
+                <Item.Group divided>
+                  <Item>
+                    <Avatar />
+
+                    <Item.Content>
+                      <Item.Header>{data ? data.me.name : ""}</Item.Header>
+                      <Item.Meta>
+                        <span className="cinema">Member since 2019</span>
+                      </Item.Meta>
+                      <Item.Description>{userLocation}</Item.Description>
+                      <Item.Extra>
+                        <Label>IMAX</Label>
+                        <Label icon="globe" content="Additional Languages" />
+                      </Item.Extra>
+                    </Item.Content>
+                  </Item>
+                </Item.Group>
               </>
             );
           }}
