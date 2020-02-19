aa1262330f2c0d89b17ed8a5f1f0bc380a4b8c9c
Exact Staff Git <admin@exactstaff.com>
2020-02-19 10:58:44 -0800

    Refactor tabs for user profile

--- pages/me.js
+++ pages/me.js
@@ -1,3 +1,4 @@
+import SEO from "../components/SEO";
 import WithAuth from "../components/hoc/WithAuth";
 import PageSection from "../components/common/Layout/PageSection";
 import Container from "../components/common/Layout/Container";
@@ -7,6 +8,10 @@ import UserProfileHeader from "../components/me/UserProfileHeader";
 const MePage = props => {
   return (
     <PageSection className="DashboardPage" column>
+      <SEO
+        description="Login to your profile to start your job search with myexactjobs. Browse through hundreds of job openings nationally. Exact Staff has the job opportunity you have been looking for so Apply Today!"
+        title="User Profile - Exact Staff National Job Board: Find a Job Today! "
+      />
       <Container>
         <UserProfileHeader />
         <UserProfileTabs />
