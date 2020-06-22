2a4679170977b2397ede2bdb9fc68d2f2e67694e
Kervin <kervinlvh@gmail.com>
2020-06-22 12:50:56 -0700

    refactor table data loading

--- components/common/UI/Table.js
+++ components/common/UI/Table.js
@@ -17,84 +17,90 @@ const TableWithPagination = ({
     turnPageHandler(activePage);
   };
 
-  if (!data) return <p>No Data To Show</p>;
+  if (!data) return <p>Loading</p>;
   return (
     <div className="CustomTable">
       <div className="CustomTable__tooblar">{toolbar}</div>
-      <Table selectable>
-        <Table.Header>
-          <Table.Row>
-            {Object.keys(data[0])
-              .filter(
-                header =>
-                  ((withid && header === "id") ||
-                    (header !== "__typename" && header !== "id")) &&
-                  !exclude.includes(header)
-              )
-              .map((header, key) => (
-                <Table.HeaderCell key={header + key}>
-                  {jsUcfirst(header)}
-                </Table.HeaderCell>
-              ))}
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          {loading ? (
+      {data.length === 0 && <p>No data to show</p>}
+      {data.length > 0 && (
+        <Table selectable>
+          <Table.Header>
             <Table.Row>
-              <Table.Cell>
-                <Loader active inline="centered" />
-              </Table.Cell>
+              {Object.keys(data[0])
+                .filter(
+                  header =>
+                    ((withid && header === "id") ||
+                      (header !== "__typename" && header !== "id")) &&
+                    !exclude.includes(header)
+                )
+                .map((header, key) => (
+                  <Table.HeaderCell key={header + key}>
+                    {jsUcfirst(header)}
+                  </Table.HeaderCell>
+                ))}
             </Table.Row>
-          ) : (
-            data.map((row, key) => (
-              <Table.Row key={"Row" + key}>
-                {Object.keys(row)
-                  .filter(
-                    rowName =>
-                      ((withid && rowName === "id") ||
-                        (rowName !== "__typename" && rowName !== "id")) &&
-                      !exclude.includes(rowName)
-                  )
-                  .map((column, index) => {
-                    return (
-                      <Table.Cell key={"Column" + key + column + index}>
-                        {row[column]}
-                      </Table.Cell>
-                    );
-                  })}
-              </Table.Row>
-            ))
-          )}
-        </Table.Body>
+          </Table.Header>
 
-        <Table.Footer>
-          <Table.Row>
-            <Table.HeaderCell colSpan={Object.keys(data[0]).length}>
-              <Pagination
-                floated="right"
-                defaultActivePage={page}
-                ellipsisItem={{
-                  content: <Icon name="ellipsis horizontal" />,
-                  icon: true
-                }}
-                firstItem={{
-                  content: <Icon name="angle double left" />,
-                  icon: true
-                }}
-                lastItem={{
-                  content: <Icon name="angle double right" />,
-                  icon: true
-                }}
-                prevItem={{ content: <Icon name="angle left" />, icon: true }}
-                nextItem={{ content: <Icon name="angle right" />, icon: true }}
-                totalPages={pages}
-                onPageChange={handlePaginationChange}
-              />
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Footer>
-      </Table>
+          <Table.Body>
+            {loading ? (
+              <Table.Row>
+                <Table.Cell>
+                  <Loader active inline="centered" />
+                </Table.Cell>
+              </Table.Row>
+            ) : (
+              data.map((row, key) => (
+                <Table.Row key={"Row" + key}>
+                  {Object.keys(row)
+                    .filter(
+                      rowName =>
+                        ((withid && rowName === "id") ||
+                          (rowName !== "__typename" && rowName !== "id")) &&
+                        !exclude.includes(rowName)
+                    )
+                    .map((column, index) => {
+                      return (
+                        <Table.Cell key={"Column" + key + column + index}>
+                          {row[column]}
+                        </Table.Cell>
+                      );
+                    })}
+                </Table.Row>
+              ))
+            )}
+          </Table.Body>
+
+          <Table.Footer>
+            <Table.Row>
+              <Table.HeaderCell colSpan={Object.keys(data[0]).length}>
+                <Pagination
+                  floated="right"
+                  defaultActivePage={page}
+                  ellipsisItem={{
+                    content: <Icon name="ellipsis horizontal" />,
+                    icon: true
+                  }}
+                  firstItem={{
+                    content: <Icon name="angle double left" />,
+                    icon: true
+                  }}
+                  lastItem={{
+                    content: <Icon name="angle double right" />,
+                    icon: true
+                  }}
+                  prevItem={{ content: <Icon name="angle left" />, icon: true }}
+                  nextItem={{
+                    content: <Icon name="angle right" />,
+                    icon: true
+                  }}
+                  totalPages={pages}
+                  onPageChange={handlePaginationChange}
+                />
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Footer>
+        </Table>
+      )}
       <style jsx>{`
         .CustomTable__tooblar {
           margin-bottom: 20px;
