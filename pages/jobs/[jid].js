79c0a641545600b3226cbc53a178cabef5236cfc
Kervin Vasquez <kvasquez@exactstaff.com>
2020-06-24 16:57:57 -0700

    enable unstable revalidation

--- pages/jobs/[jid].js
+++ pages/jobs/[jid].js
@@ -74,7 +74,7 @@ export async function getStaticProps({ params }) {
   const jobId = extractJobId(jid);
   const jobData = await getJob(jobId);
 
-  return { props: { jobData, jobId } };
+  return { props: { jobData, jobId }, unstable_revalidate: 1 };
 }
 
 function extractJobId(slug) {
