041dcdb2f868ccfb127ed741572c87efc5c306b4
Kervin Vasquez <kvasquez@exactstaff.com>
2020-07-14 17:52:09 -0700

    fix issue with download button

--- components/common/UI/DownloadCSVButton.js
+++ components/common/UI/DownloadCSVButton.js
@@ -1,33 +1,45 @@
 import React, { useState, useEffect } from "react";
-import { useLazyQuery } from "@apollo/client";
+import { useLazyQuery, ApolloConsumer } from "@apollo/client";
 import { CSVDownload } from "react-csv";
+
 import { Button } from "semantic-ui-react";
 import appText from "@/lang/appText";
 
-const DownloadCSVButton = ({ queryData }) => {
+const ActionButton = ({ client, queryData }) => {
   const [fetchedData, setFetchedData] = useState(null);
-  const [getData, { error, loading, data }] = useLazyQuery(queryData?.query, {
-    variables: queryData?.variables,
-  });
+  const [loading, setLoading] = useState(false);
+  // const [getData, { error, loading, data }] = useLazyQuery(queryData?.query, {
+  //   variables: queryData?.variables,
+  // });
 
   const handleClick = () => {
-    getData();
-    updateFetchedData();
-  };
-
-  useEffect(() => {
-    if (data) {
-      updateFetchedData();
-    }
-  }, [error, loading, data]);
-
-  const updateFetchedData = () => {
+    setLoading(true);
     setTimeout(() => setFetchedData(null), 0);
-    if (data) {
-      const [dataKey] = Object.keys(data);
-      setTimeout(() => setFetchedData(data[dataKey]), 10);
-    }
+    client
+      .query({ query: queryData.query, variables: queryData.variables })
+      .then(({ data }) => {
+        const [dataKey] = Object.keys(data);
+        setTimeout(() => setFetchedData(data[dataKey]), 10);
+        setLoading(false);
+        setTimeout(() => setFetchedData(false), 1000);
+      });
+    //   getData();
+    //   updateFetchedData();
   };
+
+  // useEffect(() => {
+  //   if (data) {
+  //     updateFetchedData();
+  //   }
+  // }, [error, loading, data]);
+
+  // const updateFetchedData = () => {
+  //   setTimeout(() => setFetchedData(null), 0);
+  //   if (data) {
+  //     const [dataKey] = Object.keys(data);
+  //     setTimeout(() => setFetchedData(data[dataKey]), 10);
+  //   }
+  // };
 
   return (
     <>
@@ -41,6 +53,16 @@ const DownloadCSVButton = ({ queryData }) => {
       </Button>
       {fetchedData && <CSVDownload data={fetchedData} target="_blank" />}
     </>
+  );
+};
+
+const DownloadCSVButton = ({ queryData }) => {
+  return (
+    <ApolloConsumer>
+      {(apolloClient) => {
+        return <ActionButton queryData={queryData} client={apolloClient} />;
+      }}
+    </ApolloConsumer>
   );
 };
 
