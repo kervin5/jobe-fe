a1d7b877faae5c9393a3b0bb2821854cedb611ef
Kervin <kervinlvh@gmail.com>
2021-04-18 22:24:06 -0700

    udpate

--- components/applications/AddNoteToApplicationForm.jsx
+++ components/applications/AddNoteToApplicationForm.jsx
@@ -3,7 +3,6 @@ import { useMutation } from "@apollo/client";
 import { useForm } from "react-hook-form";
 
 import Button from "@material-ui/core/Button";
-import TextField from '@material-ui/core/TextField';
 import { gql } from "@apollo/client";
 import styled from "styled-components";
 import { APPLICATION_NOTES_QUERY } from "./ApplicationHistoryFeed";
